afc12227605a8ac5084b08bb6f2454bae84a292c
ankush-bhawar <ankush.bhawar07@gmail.com>
2022-04-30 23:30:14 +0530

    nested if else in python

---
+++ 01-PYTHON/01-INTERMEDIATE-PYTHON/03-nested-if-else-statement.py
@@ -0,0 +1,16 @@
+if(True):
+    if(True):
+        print("this is inner if block")
+    else:
+        print("this is inner else block")
+else:
+    print("this is outer else block")
+
+a = 5
+if(a > 0):
+    if(a % 2 == 0):
+        print("a is even number")
+    else:
+        print("a is odd number")
+else:
+    print("a is negative number")
